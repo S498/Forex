67eab919803e97277118f6d2cd43992c31f699da
S498 <sathwik.edupuganti@gmail.com>
2019-03-30 16:51:03 +0530

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-var from=document.getElementById('from').value;
-var to=document.getElementById('to').value;
-
-function convert(){
-    var XHR=new XHRRequest();
-    var url="http://api.fixer.io/latest?symbols="+from+","+to;
-    XHR.open("GET",url,true);
+function convert() {
+    var fromAmount = document.getElementById('fromAmt').value;
+    console.log(fromAmount);
+    var toAmount = document.getElementById('toAmt').value;
+    var from = document.getElementById('fromCur').value;
+    var to = document.getElementById('toCur').value;
+    var XHR = new XMLHttpRequest();
+    var code = from + "_" + to;
+    var url = "https://free.currencyconverterapi.com/api/v6/convert?q=" + code + "&compact=ultra&apiKey=8a6ea31b37387bd5df83";
+    XHR.open("GET", url, true);
     XHR.send();
-    XHR.onreadystatechange=function(){
-        if(XHR.readyState==4&&XHR.status==200){
-            var result=XHR.responseText;
-            alert(result);
-            var jsResult=JSON.parse(result);
+    XHR.onreadystatechange = function () {
+        if (XHR.readyState == 4 && XHR.status == 200) {
+            var result = XHR.responseText;
+            var jsResult = JSON.parse(result);
+            var conversion = jsResult[code];
+            //                                    console.log(conversion);
+            var value = fromAmount * conversion;
+            console.log(value);
+            document.getElementById('toAmt').value = value;
         }
     }
 }
